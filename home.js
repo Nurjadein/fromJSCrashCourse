3115671dbdedbe3f21e5d30087471a245e573fd7
Nurjadein <nurjadeinabdulmorid@gmail.com>
2021-12-28 00:33:11 +0800

    variables, getElement and promt

---
+++ home.js
@@ -0,0 +1,14 @@
+// variables
+var b = 'some variable';
+var num = 30;
+
+console.log(b);
+console.log(num);
+
+// grabbing element using their ID
+document.getElementById('someText').innerHTML = 'This is a text';
+
+// promt
+var age = prompt('How old are you?');
+document.getElementById('addtionOfPromt').innerHTML = 'You are ' +age+ ' years old.';
+
