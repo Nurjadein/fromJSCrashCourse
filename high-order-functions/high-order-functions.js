fcf6119e435e36d8ab6362a60554fb67458b1c3c
Nurjadein <nurjadeinabdulmorid@gmail.com>
2022-03-29 21:10:48 +0800

    declaring 2 arrays

---
+++ high-order-functions/high-order-functions.js
@@ -0,0 +1,82 @@
+const companies = [
+  {
+    name: "Global Chemicals",
+    category: "Technology",
+    start: "1981",
+    end: "2003",
+  },
+  {
+    name: "Global Chemicals",
+    category: "Technology",
+    start: "1981",
+    end: "2003",
+  },
+  {
+    name: "Global Chemicals",
+    category: "Technology",
+    start: "1981",
+    end: "2003",
+  },
+  {
+    name: "Global Chemicals",
+    category: "Technology",
+    start: "1981",
+    end: "2003",
+  },
+  {
+    name: "Global Chemicals",
+    category: "Technology",
+    start: "1981",
+    end: "2003",
+  },
+  {
+    name: "Global Chemicals",
+    category: "Technology",
+    start: "1981",
+    end: "2003",
+  },
+  {
+    name: "Global Chemicals",
+    category: "Technology",
+    start: "1981",
+    end: "2003",
+  },
+  {
+    name: "Global Chemicals",
+    category: "Technology",
+    start: "1981",
+    end: "2003",
+  },
+  {
+    name: "Global Chemicals",
+    category: "Technology",
+    start: "1981",
+    end: "2003",
+  },
+  {
+    name: "Global Chemicals",
+    category: "Technology",
+    start: "1981",
+    end: "2003",
+  },
+  {
+    name: "Global Chemicals",
+    category: "Technology",
+    start: "1981",
+    end: "2003",
+  },
+  {
+    name: "Global Chemicals",
+    category: "Technology",
+    start: "1981",
+    end: "2003",
+  },
+  {
+    name: "Global Chemicals",
+    category: "Technology",
+    start: "1981",
+    end: "2003",
+  },
+];
+
+const ages = [33, 43, 56, 78, 35, 67, 12, 34, 32, 43, 15, 69, 45];
