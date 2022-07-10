f29674544d3009cae85947cf5289e92844e3885c
Nurjadein <nurjadeinabdulmorid@gmail.com>
2022-07-10 17:37:28 +0800

    disable promt

--- intro/home.js
+++ intro/home.js
@@ -1,3 +1,4 @@
+
 // variables
 var b = 'some variable';
 var num = 30;
@@ -9,11 +10,11 @@ console.log(num);
 document.getElementById('someText').innerHTML = 'This is a text';
 
 // promt
-var age = prompt('How old are you?');
-if(age == null || age == '')
-document.getElementById('addtionOfPromt').innerHTML = 'You cannot have an emty age.';
-else
-document.getElementById('addtionOfPromt').innerHTML = 'You are ' +age+ ' years old.';
+// var age = prompt('How old are you?');
+// if(age == null || age == '')
+// document.getElementById('addtionOfPromt').innerHTML = 'You cannot have an emty age.';
+// else
+// document.getElementById('addtionOfPromt').innerHTML = 'You are ' +age+ ' years old.';
 
 // functions
  function add(num1,num2){
